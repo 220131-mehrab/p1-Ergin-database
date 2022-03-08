440d22741a5a0059da2ff366c42e70bb9e216f82
erginbostanci <erginbostanci@yahoo.com>
2022-03-07 22:31:03 -0600

    Uploaded the List from to Browser

--- src/main/java/com/revature/database/App.java
+++ src/main/java/com/revature/database/App.java
@@ -1,7 +1,6 @@
 package com.revature.database;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,9 +10,9 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.PortUnreachableException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -22,90 +21,105 @@ import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+class Artist {
+    private int artistId;
+    private String name;
+    public Artist(int artistId, String name) {
+        this.artistId = artistId;
+        this.name = name;
+    }
+    public Artist() {
+    }
+    public int getArtistId() {
+        return artistId;
+    }
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Artist [artistId=" + artistId + ", name=" + name + "]";
+    }
+}
+
 public class App {
     public static void main(String[] args) throws SQLException {
-        //This line is for to make a DB connection to SQL and run the script in schema.sql
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;INIT=runscript from 'classpath:schema.sql'", "sa","");
-        //after the above line we run the query and get the entire set of data printed and stored in rs as a resultset
-        ResultSet rs = connection.prepareStatement("select * from Artist").executeQuery();
-        //Next line creation is before the loop since we will create the list that way
-        List<String> artists = new ArrayList<>();
-        //we will loop through the data now and reading each letter
-        while (rs.next()){
-            artists.add(rs.getString("Name"));
-        }
-        // Creating an HttpServlet
+
         HttpServlet artistServlet = new HttpServlet() {
             @Override
             protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                     throws ServletException, IOException {
-            //Getting a json mapper
-            ObjectMapper mapper = new ObjectMapper();
-            //we use the mapper to get the artist names stored in it, and store that as another variable results pass it on
-            String results = mapper.writeValueAsString(artists);
-            //we are setting the content type to json. This maybe important since the api call we will make requires that
-            resp.setContentType("application/json");
-            //when we want to print from a mapper always used getWriter
-            resp.getWriter().println(results);
+            List<Artist> artists = new ArrayList<>();
+            try {
+                ResultSet rs = connection.prepareStatement("select * from Artist").executeQuery();
+            while (rs.next()){
+                artists.add(new Artist(rs.getInt("ArtistId"), rs.getString("Name")));
+                }
+            } catch (SQLException e) {
+                System.err.println("Failed to retrieve from DB: " + e.getSQLState());
             }
+
+            ObjectMapper mapper = new ObjectMapper();
+            String results = mapper.writeValueAsString(artists);
+            resp.setContentType("application/json");
+            resp.getWriter().println(results);
+        }
 
             @Override
             protected void doPost(HttpServletRequest req, HttpServletResponse resp)
                     throws ServletException, IOException {
+
                 ObjectMapper mapper = new ObjectMapper();
-                String newArtist = mapper.readValue(req.getInputStream(), String.class);
+                Artist newArtist = mapper.readValue(req.getInputStream(), Artist.class);
+                System.out.println(newArtist);
                 try {
-                    PreparedStatement stmt = connection.prepareStatement("insert into 'artist' values (?,?)")
-                    stmt.setInt(1, 3);
+                    PreparedStatement stmt = connection.prepareStatement("insert into 'artist' values (?,?)");
+                    stmt.setInt(1, newArtist.getArtistId());
+                    stmt.setString(2, newArtist.getName());
+                    stmt.executeUpdate();
                 } catch (SQLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    System.err.println("Failed to insert" + e.getMessage());
                 }
             }
         };
-
         Tomcat server = new Tomcat();
-        
-        //This will start the localhost 8080 Connector
         server.getConnector();
         server.addContext("",null);
-        
-        //Ananymous Class
         server.addServlet("", "defaultServlet", new HttpServlet() {
             @Override
             protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                 System.out.println("This is the path information requested from doGet" + req.getPathInfo());
-                String filename = req.getPathInfo();//.replaceFirst("/","");
+                String filename = req.getPathInfo();
                 String resourcesDir = "static";
-                //This part is to load the file. It also requires
                 InputStream file = getClass().getClassLoader().getResourceAsStream(resourcesDir + filename);
-                //Question: is Mime type convert the file type or modifies it or leaves it the same but just inform the Http
                 String mimeType = getServletContext().getMimeType(filename);
                 resp.setContentType(mimeType);
-                //Question: By copying the file with the response, what are we doing exactly?
                 IOUtils.copy(file,resp.getOutputStream());
-                //super.doGet(req, resp);
             }
         }).addMapping("/*");
-        //next line we are adding the information printed in the above artistServlet and passing it on to the server
         server.addServlet("", "artistServlet", artistServlet).addMapping("/artists");;//Question:why both of them are supposed to be the same
         try {
             server.start();
         } catch (LifecycleException e) {
-            //There extra println error messages for me to see what kind of error messages will be printed
             System.err.println("Failed to start server in the main App :"+ e.getMessage());
-            System.err.println("Failed to start server in the main App :"+ e.getLocalizedMessage());
-            System.err.println("Failed to start server in the main App :"+ e.getCause());
-            System.err.println("Failed to start server in the main App :"+ e.getStackTrace());
-            System.err.println("Failed to start server in the main App :"+ e.getSuppressed());
             e.printStackTrace();
         }
     }
+
     static void Wait(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Press any key to continue");
         if (scan.nextLine() == " "){
             System.out.println("OK lets move on");
+            scan.close();
         } else System.out.println("press space");
+        
     }
 }
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,3 +1,49 @@
 var welcomeMsg = '  This is the welcomeMsg and there are two spaces in the beginning can you see that'
 
-document.querySelector('h1').innerText = welcomeMsg
+document.querySelector('h1').innerText = welcomeMsg;
+
+fetch('/artists').then(resp => resp.json()).then(artists =>{
+    document.querySelector('body').innerHTML = listArtists(artists)
+
+});
+    let listArtist = function(artist) {
+    return '<p>' + artist.artistId + ": " + artist.name + '</p>';
+}
+
+function listArtists(json) {
+    return`
+        <div id="artistList">
+            ${json.map(listArtist).join('\n')}
+        </div>
+    `
+}
+
+// function get(url){
+//     return new Promise(function(resolve, reject){
+//         var req = new XMLHttpRequest();
+//         req.open('GET',url);
+//         req.onload =  function() {
+//             if (req.status == 200){
+//                 resolve(req.response);
+//             } else {
+//                 reject(Error(req.statusText));
+//             }
+//     }
+//         req.onerror = function() {
+//             reject(Error("Network error"));
+//         }
+//         req.send();
+//     }
+//     )}
+
+// get('/artists').then(function(response) {
+//     console.log(JSON.parse(response))
+
+// },function(err){
+
+// })
+
+
+
+
+
